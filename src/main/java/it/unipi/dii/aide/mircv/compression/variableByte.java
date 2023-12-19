fdac43c7e68356c58d4114c6c04fff83682c3369
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-19 12:05:48 +0100

    class created

--- src/main/java/it/unipi/dii/aide/mircv/compression/variableByte.java
+++ src/main/java/it/unipi/dii/aide/mircv/compression/variableByte.java
@@ -1,4 +1,107 @@
 package it.unipi.dii.aide.mircv.compression;
+import java.util.ArrayList;
+
 
 public class variableByte {
+
+    // compress an array of integer
+    public static byte[] compress(ArrayList<Integer> uncompressed) {
+
+        ArrayList<Integer> compressed = new ArrayList<>();
+
+        for(int uncompressedNumber : uncompressed) {
+
+            // convert the number to binary
+            String binary = Integer.toBinaryString(uncompressedNumber);
+
+            // add 0s at the beginning to complete the byte
+            while(binary.length() % 7 != 0) {
+                binary = "0" + binary;
+            }
+
+            // split the binary string in 7-bit chunks removing the first bit
+            for(int i = 0; i < binary.length(); i += 7) {
+                // check if the chunk is the first one
+                if(i == 0) {
+                    // add a 0 at the beginning
+                    compressed.add(Integer.parseInt("0" + binary.substring(i, i + 7), 2));
+                } else {
+                    // add a 1 at the beginning
+                    compressed.add(Integer.parseInt("1" + binary.substring(i, i + 7), 2));
+                }
+            }
+        }
+
+        // convert the arraylist to a byte array
+        byte[] compressedBytes = new byte[compressed.size()];
+        for(int i = 0; i < compressed.size(); i++) {
+            compressedBytes[i] = compressed.get(i).byteValue();
+        }
+
+        return compressedBytes;
+
+    }
+
+    // decompress an array of bytes
+    public static ArrayList<Integer> decompress(byte[] compressed) {
+
+        ArrayList<Integer> decompressed = new ArrayList<>();
+        StringBuilder binaryBuilder = new StringBuilder();
+        StringBuilder supportBuilder = new StringBuilder();
+
+        int decodedNumber;
+
+        // convert the bytes to a binary string
+        for (byte b : compressed) {
+            binaryBuilder.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+        }
+
+        // decoding the binary string
+        while (binaryBuilder.length() > 0) {
+            int firstBit = Character.getNumericValue(binaryBuilder.charAt(0));
+
+            // if the first bit is 0, it is the first chunk of the number
+            if (firstBit == 0) {
+                int groupLength = Math.min(8, binaryBuilder.length());
+                String group = binaryBuilder.substring(0, groupLength);
+                binaryBuilder.delete(0, groupLength);
+
+                // check if the support builder is empty
+                if (supportBuilder.length() == 0) {
+                    // add the binary group to the support builder
+                    supportBuilder.append(group);
+                }
+                else {
+                    // convert support builder to an integer
+                    decodedNumber = Integer.parseInt(supportBuilder.toString(), 2);
+                    decompressed.add(decodedNumber);
+                    // reset the support builder
+                    supportBuilder.setLength(0);
+                    // add the binary group to the support builder
+                    supportBuilder.append(group);
+                }
+
+            } else {
+                // if the first bit is 1, it is not the first chunk of the number
+                int groupLength = Math.min(7, binaryBuilder.length() - 1);
+                String group = binaryBuilder.substring(1, 1 + groupLength);
+                binaryBuilder.delete(0, 1 + groupLength);
+
+                // add a 1 at the beginning of the group
+                //group = "1" + group;
+
+                // add the binary group to the support builder at the end
+                supportBuilder.append(group);
+            }
+        }
+
+        // convert support builder to an integer
+        decodedNumber = Integer.parseInt(supportBuilder.toString(), 2);
+        decompressed.add(decodedNumber);
+        // reset the support builder
+        supportBuilder.setLength(0);
+
+        return decompressed;
+    }
+
 }
---
+++ src/test/java/it/unipi/dii/aide/mircv/compression/variableByteTest.java
@@ -0,0 +1,89 @@
+package it.unipi.dii.aide.mircv.compression;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class variableByteTest {
+
+    @Test
+    void compress() {
+        // Test input array: [67822]
+        // Expected compressed binary: 00000100 00010001 11101110
+        ArrayList<Integer> input1 = new ArrayList<>();
+        input1.add(67822);
+
+        // Test input array: [1, 2, 4]
+        // Expected compressed binary: 00000001 00000010 00000100
+        ArrayList<Integer> input2 = new ArrayList<>();
+        input2.add(1);
+        input2.add(2);
+        input2.add(4);
+
+        // Test input array: [1, 2, 67822, 4]
+        // Expected compressed binary: 00000001 00000010 00000100 00010001 11101110 00000100
+        ArrayList<Integer> input3 = new ArrayList<>();
+        input3.add(1);
+        input3.add(2);
+        input3.add(67822);
+        input3.add(4);
+
+        // Expected compressed bytes
+        byte[] expectedCompressed1 = {(byte) 0b00000100, (byte) 0b10010001, (byte) 0b11101110};
+        byte[] expectedCompressed2 = {(byte) 0b00000001, (byte) 0b00000010, (byte) 0b00000100};
+        byte[] expectedCompressed3 = {(byte) 0b00000001, (byte) 0b00000010, (byte) 0b00000100, (byte) 0b10010001, (byte) 0b11101110, (byte) 0b00000100};
+
+        // Compress the input
+        byte[] compressed1 = variableByte.compress(input1);
+        byte[] compressed2 = variableByte.compress(input2);
+        byte[] compressed3 = variableByte.compress(input3);
+
+        // Assert that the compressed array is equal to the expected compressed array
+        //assertArrayEquals(expectedCompressed1, compressed1);
+        //assertArrayEquals(expectedCompressed2, compressed2);
+        assertArrayEquals(expectedCompressed3, compressed3);
+
+    }
+
+    @Test
+    void decompress() {
+        // Test input array: [67822]
+        // Expected compressed binary: 00000100 10010001 11101110
+        ArrayList<Integer> expectedDecompressed1 = new ArrayList<>();
+        expectedDecompressed1.add(67822);
+
+        // Test input array: [1, 2, 4]
+        // Expected compressed binary: 00000001 00000010 00000100
+        ArrayList<Integer> expectedDecompressed2 = new ArrayList<>();
+        expectedDecompressed2.add(1);
+        expectedDecompressed2.add(2);
+        expectedDecompressed2.add(4);
+
+        // Test input array: [1, 2, 67822, 4]
+        // Expected compressed binary: 00000001 00000010 00000100 10010001 11101110 00000100
+        ArrayList<Integer> expectedDecompressed3 = new ArrayList<>();
+        expectedDecompressed3.add(1);
+        expectedDecompressed3.add(2);
+        expectedDecompressed3.add(67822);
+        expectedDecompressed3.add(4);
+
+        // Compress the input
+        byte[] compressed1 = {(byte) 0b00000100, (byte) 0b10010001, (byte) 0b11101110};
+        byte[] compressed2 = {(byte) 0b00000001, (byte) 0b00000010, (byte) 0b00000100};
+        byte[] compressed3 = {(byte) 0b00000001, (byte) 0b00000010, (byte) 0b00000100, (byte) 0b10010001, (byte) 0b11101110, (byte) 0b00000100};
+
+
+        // Decompress the compressed array
+        ArrayList<Integer> decompressed1 = variableByte.decompress(compressed1);
+        ArrayList<Integer> decompressed2 = variableByte.decompress(compressed2);
+        ArrayList<Integer> decompressed3 = variableByte.decompress(compressed3);
+
+        // Assert that the decompressed array is equal to the expected decompressed array
+        //assertEquals(expectedDecompressed1, decompressed1);
+        //assertEquals(expectedDecompressed2, decompressed2);
+        assertEquals(expectedDecompressed3, decompressed3);
+    }
+
+}
