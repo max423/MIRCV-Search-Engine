7c8ebe654dfdfa15bf3b5050f896f2587cd26afa
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-20 16:49:23 +0100

    class created

---
+++ src/main/java/it/unipi/dii/aide/mircv/query/queryHandler.java
@@ -0,0 +1,9 @@
+package it.unipi.dii.aide.mircv.query;
+
+import it.unipi.dii.aide.mircv.models.VocabularyElem;
+
+// receive a query and return the top k (10 or 20) results
+public class queryHandler {
+
+
+}
