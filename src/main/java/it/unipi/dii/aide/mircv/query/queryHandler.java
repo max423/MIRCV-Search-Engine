56461c315e986129072b992c457cfe7c7eaf4315
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-21 17:02:53 +0100

    class updated

--- src/main/java/it/unipi/dii/aide/mircv/query/queryHandler.java
+++ src/main/java/it/unipi/dii/aide/mircv/query/queryHandler.java
@@ -1,9 +1,8 @@
 package it.unipi.dii.aide.mircv.query;
 
 import it.unipi.dii.aide.mircv.models.PostingList;
-import it.unipi.dii.aide.mircv.models.Vocabulary;
-import it.unipi.dii.aide.mircv.models.VocabularyElem;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 // receive a query and return the top k (10 or 20) results
@@ -29,4 +28,33 @@ public class queryHandler {
 
 
     }*/
+
+    // receive a query and return the top k (10 or 20) results
+    public static void executeQuery(ArrayList<String> tokens, int k) throws IOException {
+
+        int position = 0;
+
+        // ArrayList of tokens removing duplicates
+        ArrayList<String> tokensNoDuplicates = new ArrayList<>();
+        for (String token : tokens) {
+            if (!tokensNoDuplicates.contains(token)) {
+                tokensNoDuplicates.add(token);
+            }
+        }
+
+        // process each token of the query
+        for (String token : tokensNoDuplicates) {
+
+            // posting list initialization
+            PostingList postingList = new PostingList();
+            postingList.getPostingList().clear();
+            postingList.setTerm(token);
+
+            // obtain the posting list for the token
+            postingList = PostingList.getPostingList(token);
+
+
+        }
+    }
+
 }
