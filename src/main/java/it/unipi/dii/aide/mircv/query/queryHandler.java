11998f0f59c9a8868f4408bf1f38babeb5e27760
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-21 12:35:06 +0100

    calss created

--- src/main/java/it/unipi/dii/aide/mircv/query/queryHandler.java
+++ src/main/java/it/unipi/dii/aide/mircv/query/queryHandler.java
@@ -1,9 +1,32 @@
 package it.unipi.dii.aide.mircv.query;
 
+import it.unipi.dii.aide.mircv.models.PostingList;
+import it.unipi.dii.aide.mircv.models.Vocabulary;
 import it.unipi.dii.aide.mircv.models.VocabularyElem;
+
+import java.util.ArrayList;
 
 // receive a query and return the top k (10 or 20) results
 public class queryHandler {
 
+    private static final Vocabulary vocabulary = Vocabulary.getInstance();
+    // load the posting list of the query terms from disk
+    /*public static ArrayList<PostingList> loadPostingLists(ArrayList<String> tokens, boolean conjunctive) {
 
+        // ArrayList of tokens removing duplicates
+        ArrayList<String> tokensNoDuplicates = new ArrayList<>();
+        for (String token : tokens) {
+            if (!tokensNoDuplicates.contains(token)) {
+                tokensNoDuplicates.add(token);
+            }
+        }
+
+        // for each token, load the posting list from disk
+        ArrayList<PostingList> postingLists = new ArrayList<>();
+        for (String token : tokensNoDuplicates) {
+            postingLists.add(PostingList.loadPostingList(token));
+        }
+
+
+    }*/
 }
