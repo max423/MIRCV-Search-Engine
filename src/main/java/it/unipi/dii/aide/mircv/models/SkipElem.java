453d1d18653d6279a9801ea0ceed5a61caa9393c
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-27 12:15:53 +0100

    Values added

--- src/main/java/it/unipi/dii/aide/mircv/models/SkipElem.java
+++ src/main/java/it/unipi/dii/aide/mircv/models/SkipElem.java
@@ -1,5 +1,12 @@
 package it.unipi.dii.aide.mircv.models;
 
+import it.unipi.dii.aide.mircv.utils.FileUtils;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class SkipElem {
 
     // max docID of the block
@@ -35,5 +42,44 @@ public class SkipElem {
 
     public void setDocID(int docID) {
         this.docID = docID;
+    }
+
+    // read skip element from disk
+    public void readFromDisk(FileChannel skipRaf, long skipAddress) throws IOException {
+        // allocate the size of the skip element
+        ByteBuffer skipBuffer= ByteBuffer.allocate(4+8+4+8+4);
+
+        // start position
+        skipRaf.position(skipAddress);
+
+        // read the skip element
+        while (skipBuffer.hasRemaining()) {
+            skipRaf.read(skipBuffer);
+        }
+
+        // reset the buffer
+        skipBuffer.rewind();
+        // get skip info
+        this.setDocID(skipBuffer.getInt());
+        this.setOffsetDocID(skipBuffer.getLong());
+        this.setBlockDocIDLen(skipBuffer.getInt());
+        this.setOffsetFreq(skipBuffer.getLong());
+        this.setBlockFreqLen(skipBuffer.getInt());
+    }
+
+    private void setBlockFreqLen(int blockFreqLen) {
+        this.blockFreqLen = blockFreqLen;
+    }
+
+    private void setOffsetFreq(long offsetFreq) {
+        this.offsetFreq = offsetFreq;
+    }
+
+    private void setBlockDocIDLen(int blockDocIDLen) {
+        this.blockDocIDLen = blockDocIDLen;
+    }
+
+    private void setOffsetDocID(long offsetDocID) {
+        this.offsetDocID = offsetDocID;
     }
 }
