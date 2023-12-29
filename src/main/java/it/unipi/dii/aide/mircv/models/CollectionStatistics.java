590d76ede2a7867e35a8a1db8ac5b167426c7ebe
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-29 12:05:04 +0100

    update dimension

--- src/main/java/it/unipi/dii/aide/mircv/models/CollectionStatistics.java
+++ src/main/java/it/unipi/dii/aide/mircv/models/CollectionStatistics.java
@@ -6,7 +6,7 @@ import java.nio.channels.FileChannel;
 
 public class CollectionStatistics {
     // # of documents = size of documnetIndex
-    private static long docCount;
+    private static int docCount;
     // total number of terms
     private static long totalLength;
 
@@ -16,12 +16,12 @@ public class CollectionStatistics {
         this.totalLength = 0;
     }
 
-    public CollectionStatistics(long docCount, long totalLength) {
+    public CollectionStatistics(int docCount, long totalLength) {
         this.docCount = docCount;
         this.totalLength = totalLength;
     }
 
-    public static long getDocCount() {
+    public static int getDocCount() {
         return docCount;
     }
 
@@ -52,7 +52,7 @@ public class CollectionStatistics {
     }
 
     public void writeToDisk(FileChannel channel) throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(8+8);
+        ByteBuffer buffer = ByteBuffer.allocate(4+8);
 
         channel.position(channel.size());
 
@@ -68,7 +68,7 @@ public class CollectionStatistics {
     }
 
     public void readFromDisk(FileChannel channel, long offset) throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(8+8);
+        ByteBuffer buffer = ByteBuffer.allocate(4+8);
 
         channel.position(offset);
 
@@ -76,7 +76,7 @@ public class CollectionStatistics {
             channel.read(buffer);
 
         buffer.rewind(); // reset the buffer position to 0
-        this.docCount = buffer.getLong();                        // reading docCount from buffer
+        this.docCount = buffer.getInt();                        // reading docCount from buffer
         this.totalLength = buffer.getLong();                     // reading totalLength from buffer
     }
 }
