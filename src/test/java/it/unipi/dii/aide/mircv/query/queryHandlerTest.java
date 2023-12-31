a976c0f30744b1b687de54e3beceaffc7a55592c
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2023-12-31 11:34:49 +0100

    Update queryHandler Test

--- src/test/java/it/unipi/dii/aide/mircv/query/queryHandlerTest.java
+++ src/test/java/it/unipi/dii/aide/mircv/query/queryHandlerTest.java
@@ -20,11 +20,17 @@ class queryHandlerTest {
     void executeQuery() throws IOException {
 
         // create a String as a query
-        String query = "Test query for the search engine";
+        String query1 = "First Test query for the search engine";
+        String query2 = "Second Test query for the search engine";
+        String query3 = "Third Test query for the search engine";
 
-        ArrayList<String> tokens = queryHandler.QueryPreProcessing(query);
+        ArrayList<String> tokens1 = queryHandler.QueryPreProcessing(query1);
+        ArrayList<String> tokens2 = queryHandler.QueryPreProcessing(query2);
+        ArrayList<String> tokens3 = queryHandler.QueryPreProcessing(query3);
 
-        System.out.println("Query tokens: " + tokens);
+        System.out.println("Query tokens: " + tokens1);
+        System.out.println("Query tokens: " + tokens2);
+        System.out.println("Query tokens: " + tokens3);
 
         // BM25 + Conjunctive + MaxScore
         Configuration configuration = new Configuration();
@@ -32,56 +38,72 @@ class queryHandlerTest {
         configuration.setConjunctiveON(true); // Conjunctive
         configuration.setMaxScoreON(true); // MaxScore
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // TFIDF + Conjunctive + MaxScore
         configuration.setScoreON(false); // TFIDF
         configuration.setConjunctiveON(true); // Conjunctive
         configuration.setMaxScoreON(true); // MaxScore
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // BM25 + Disjunctive + MaxScore
         configuration.setScoreON(true); // BM25
         configuration.setConjunctiveON(false); // Disjunctive
         configuration.setMaxScoreON(true); // MaxScore
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // TFIDF + Disjunctive + MaxScore
         configuration.setScoreON(false); // TFIDF
         configuration.setConjunctiveON(false); // Disjunctive
         configuration.setMaxScoreON(true); // MaxScore
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // BM25 + Conjunctive + DAAT
         configuration.setScoreON(true); // BM25
         configuration.setConjunctiveON(true); // Conjunctive
         configuration.setMaxScoreON(false); // DAAT
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // TFIDF + Conjunctive + DAAT
         configuration.setScoreON(false); // TFIDF
         configuration.setConjunctiveON(true); // Conjunctive
         configuration.setMaxScoreON(false); // DAAT
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // BM25 + Disjunctive + DAAT
         configuration.setScoreON(true); // BM25
         configuration.setConjunctiveON(false); // Disjunctive
         configuration.setMaxScoreON(false); // DAAT
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
         // TFIDF + Disjunctive + DAAT
         configuration.setScoreON(false); // TFIDF
         configuration.setConjunctiveON(false); // Disjunctive
         configuration.setMaxScoreON(false); // DAAT
 
-        queryHandler.executeQuery(tokens, 5);
+        queryHandler.executeQuery(tokens1, 5);
+        queryHandler.executeQuery(tokens2, 5);
+        queryHandler.executeQuery(tokens3, 5);
 
     }
 
